3b2ec3c4135e15dcd55a95cf78ff26b73e61d1a8
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-30 08:20:33 +0000

    up

--- src/js/component/Modal.js
+++ src/js/component/Modal.js
@@ -1,48 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import Form from "./form";
+import propTypes from "prop-types";
 
-const Modal = () => {
-    return (
-        <div className="modal" tabIndex="-1" role="dialog" style={{display: (this.props.show) ? 'inline-block' : 'none'}}>
-            <div className="modal-dialog" role="document">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title">Are you sure?</h5>
-                        { (this.props.onClose) ?
-                            <button onClick={() => this.props.onClose()} type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            :''
-                        }
-                    </div>
-                    <div className="modal-body">
-                        <p>If you delete this thing the entire universe will go down!</p>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-primary">Oh no!</button>
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Yes baby!</button>
+//create your first component
+const Modal = (props) => {
+	return (
+        <div className="container-modal">
+            <div className="modal" id={"contactModal-"+props.index} tabIndex="-1">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">Add a new contact</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <Form full_name={props.full_name} address={props.address} email={props.email} phone={props.phone} index={(props.index).toString()}/>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    );
+	);
+};
+
+Modal.propTypes = {
+    index: propTypes.string,
+    full_name: propTypes.string,
+    address: propTypes.string,
+    phone: propTypes.string,
+    email: propTypes.string,
+    img: propTypes.string
 }
 
-/**
- * here is where you define the data-types for
- * your component propersties
-**/
-Modal.propTypes = {
-    history: PropTypes.object,
-    onClose: PropTypes.func,
-    show: PropTypes.bool
-};
-
-/**
- * here is where you define the default values for
- * your component propersties
-**/
-Modal.defaultProps = {
-  show: false,
-  onClose: null
-};
-export default withRouter(Modal);
+export default Modal;
