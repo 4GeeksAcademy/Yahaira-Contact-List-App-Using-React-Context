bf4b62ee3c30bd984f4d9107edd7e4aaae96b17f
YahairaCM <63628432+Yahaira326@users.noreply.github.com>
2023-07-30 08:26:44 +0000

    up

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,36 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useContext, useEffect} from "react";
+import Contact from "../component/contact";
+import Modal from "../component/modal";
+import ConfirmationModal from "../component/confirmation-modal";
+import {Context} from "../store/appContext";
 import "../../styles/home.css";
+import golden from "../../img/golden.jpg"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const {store, actions} = useContext(Context);
+	const {contacts} = store;
+	useEffect(() => {
+		actions.getAgenda()
+	}, [])
+	return(
+		<div className="main-content text-center mt-5">
+			<Modal index={(-1).toString()}/>
+			<div className="list-group">
+				{contacts.map( (contact) =>
+					<div key={(contact.id).toString()}>
+						<Contact 
+							index={(contact.id).toString()}
+							full_name={contact.full_name} 
+							address={contact.address} 
+							email={contact.email}
+							img={golden}
+							phone={contact.phone}
+						/>
+						<Modal full_name={contact.full_name} address={contact.address} email={contact.email} phone={contact.phone} index={(contact.id).toString()} />
+						<ConfirmationModal index={contact.id}  onDelete={() => actions.delContact((contact.id).toString())}/>
+					</div>
+				)}
+			</div>
+		</div>
+	)
+};
